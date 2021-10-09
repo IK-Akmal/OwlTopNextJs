32d6d3ef9835810db143cb1de9165dca824260dc
E.N.D <isomadinovakmal1997@gmail.com>
2021-10-09 13:37:41 +0500

    initial

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,7 @@
+
+export const Htag = (): JSX.Element => {
+    return (
+        <>
+        </>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
